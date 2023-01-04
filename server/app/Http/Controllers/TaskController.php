5d1e3a32804d17b3a89cf2f1deb12ecafb5e909e
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2023-01-04 08:46:02 -0300

    :recycle: refactor: Refatora api

--- server/app/Http/Controllers/TaskController.php
+++ server/app/Http/Controllers/TaskController.php
@@ -2,51 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Task as Task;
-use App\Http\Resources\Task as TaskResource;
+use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class TaskController extends Controller
 {
-  public function index()
-  {
-    $tasks = Task::paginate(15);
-    return TaskResource::collection($tasks);
-  }
 
-  public function show($id)
-  {
-    $task = Task::findOrFail($id);
-    return new TaskResource($task);
-  }
+    public function index() {
+        $date = request("date");
 
-  public function store(Request $request)
-  {
-    $task = new Task;
-    $task->description = $request->input('description');
-    $task->date = $request->input('date');
-
-    if ($task->save()) {
-      return new TaskResource($task);
+        if ($date) {
+            $tasks = Task::where([
+                ['date', $date],
+            ])->get();
+        } else {
+            $tasks = Task::all();
+        }
+        return Response::json($tasks, 200);
     }
-  }
 
-  public function update(Request $request)
-  {
-    $task = Task::findOrFail($request->id);
-    $task->description = $request->input('description');
-    $task->date = $request->input('date');
+    public function store(Request $request) {
+        $task = new Task;
+        $task->description = $request->description;
+        $task->save();
 
-    if ($task->save()) {
-      return new TaskResource($task);
+        return Response::json([ "message" => "Tarefa criada com sucesso!"], 201);
     }
-  }
 
-  public function destroy($id)
-  {
-    $task = Task::findOrFail($id);
-    if ($task->delete()) {
-      return new TaskResource($task);
+    public function show($id) {
+        $task = Task::findOrFail($id);
+
+        return Response::json($task, 200);
     }
-  }
+
+    public function destroy($id) {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return Response::json(["message" => "Tarefa: {$task->description}, foi deletada com sucesso!"], 200);
+    }
+
+    public function update(Request $request) {
+        $task = [
+            "description" => $request->description,
+            "date" => date("y/m/d"),
+        ];
+
+        Task::where("id", $request->id)->update($task);
+
+        return Response::json(["message" => "Tarefa atualizada para: {$request->description}"], 200);
+    }
+
 }
